2aa4d5530efcfa759aed18b2273c422edca609ba
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-02 17:15:26 -0300

    Add support to CORS

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module gitlab.com/alura-courses-code/golang/go-rest-api
 go 1.21.1
 
 require (
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.1 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"gitlab.com/alura-courses-code/golang/go-rest-api/controllers"
 	"gitlab.com/alura-courses-code/golang/go-rest-api/middleware"
@@ -17,5 +18,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personality", controllers.HandleCreatePersonality).Methods("POST")
 	r.HandleFunc("/api/personality/{id}", controllers.HandleDeletePersonality).Methods("DELETE")
 	r.HandleFunc("/api/personality/{id}", controllers.HandleUpdatePersonality).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
